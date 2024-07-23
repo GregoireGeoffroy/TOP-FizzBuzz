e65104d1c89a108757aea25db7ff14f73aeaf9ef
GregoireGeoffroy <thegathering92@gmail.com>
2024-07-23 23:23:36 +0300

    Ask for number

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// When a user inputs a number
+// Loop from 1 to the entered number
+// If the current number is divisible by 3 then print "Fizz"
+// If the current number is divisible by 5 then print "Buzz"
+// If the current number is divisible by 3 and 5 then print "FizzBuzz"
+// Otherwise print the current number
+
+
+let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+// The above code should create a little popup box that asks the user for a number. The input we get back will be stored in our variable answer.
+
